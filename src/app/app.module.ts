5946596c232a403a470249705e887c3c9a66c36d
wmooseker <wmooseker@zagmail.gonzaga.edu>
2020-07-15 14:51:36 -0700

    firebase and firestore setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {environment} from '../environments/environment';
+
+import{AngularFireModule} from '@angular/fire';
+import{AngularFirestoreModule} from '@angular/fire/firestore';
+import{AngularFireAuthModule} from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    Ang
   ],
   providers: [],
   bootstrap: [AppComponent]
